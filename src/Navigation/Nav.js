64947828c0f5cc8c27ebdf81e2fa1bf7f523a3c2
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-25 08:30:51 +0300

    updated nav

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Nav.css'
+import ClearIcon from '@mui/icons-material/Clear';
+import DehazeIcon from '@mui/icons-material/Dehaze';
+
+function Nav() {
+    const[menu,setMenu]=useState(false)
+    const changeMe=()=>{
+        setMenu(!menu)
+    }
+  return (
+    <nav className='navbar'>
+        <div className='title'>M-market</div>
+        <div className='mymenu' onClick={changeMe}>
+            {menu ? <DehazeIcon className='icon'/>: <ClearIcon className='icon'/>  }
+        </div>
+        <div className={menu ? 'Dontshow' : 'show'}>
+            <ul>
+                <li><a href='#'>home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Products</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
