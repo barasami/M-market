709b558591abffe42d6a7097d18be012b20e3782
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-28 22:16:41 +0300

    added pages

---
+++ src/Data/Mydata.js
@@ -0,0 +1,12 @@
+export const CoolData=[
+    {
+        id:1,
+        name:'mik',
+        from:'kenya'
+    },
+    {
+        id:2,
+        name:'barasa',
+        from:'kenya'
+    }
+]
--- src/Myproducts/Myproducts.js
+++ src/Myproducts/Myproducts.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Myproducts.css'
 import Time from '../Timing/Time'
+import { CoolData } from '../Data/Mydata'
+
 
 function Myproducts() {
+  const[data,setData]=useState([])
+
+  useEffect(()=>{
+   setData(CoolData)
+  },[])
+  
+  const Mymap=data.map((singles)=>{
+    const{id,name,from}=singles
+    return(
+      <div key={id}>
+       {name}
+       {from}
+      </div>
+    )
+  })
+ 
   return (
     <>
       <div className='time'>
         <Time/>
       </div>
       <div className='products'>
-        hello there people
+        {Mymap}
       </div>
     </>
   )
