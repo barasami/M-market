7ce93cb8926fd31441941cdfc563611b335d7335
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-03-31 07:41:42 +0300

    modified homepg

--- src/Myproducts/Myproducts.js
+++ src/Myproducts/Myproducts.js
@@ -3,7 +3,7 @@ import './Myproducts.css'
 import Time from '../Timing/Time'
 import { CoolData } from '../Data/Mydata'
 import Paper from '@mui/material/Paper';
-import { Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 
 
@@ -20,7 +20,7 @@ function Myproducts() {
     return(
       <Grid item  lg={3} sm={6} xs={12}  key={id}  className='docs'>
         <Paper elevation={3} className='pep'>
-          <Typography mt={2} className='img'>
+          <Typography mt={1} className='img'>
             <img src={image} alt={name} className='img'/>
           </Typography>
           <div className='head'>
@@ -28,6 +28,10 @@ function Myproducts() {
           </div>
           <div className='cost'>
             Cost ${cost}
+          </div>
+          <div className='btn'>
+            <button className='mybtn'>Add</button>
+            <button className='mybtn'>Remove</button>
           </div>
        </Paper>
       </Grid>
