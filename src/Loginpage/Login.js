b604e24fc5d1a82974375e3a36e0ecb25a5a68fe
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-01 21:24:44 +0300

    modified lgin pg

--- src/Loginpage/Login.js
+++ src/Loginpage/Login.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import './Login.css'
-import TextField from '@mui/material/TextField';
+
 
 function Login() {
   return (
     <div className='login'>
       <form className='form'>
-        <TextField
-          label="Size"
-          id="outlined-size-small"
-          defaultValue="Small"
-          size="small"
-        />
+       <div>
+          <div className='market'>
+            <p className='mytitle'>M-MARKET</p>
+          </div>
+          <div className='username'>
+            <input type='text' placeholder='Username' className='user'/>
+          </div>
+          <div className='password'>
+            <input type='password' placeholder='Password' className='psw'/>
+          </div>
+          <div className='buttons'>
+            <button className='btn'>Login</button>
+            <button className='btn'>Register</button>
+          </div>
+       </div>
       </form>
     </div>
   )
