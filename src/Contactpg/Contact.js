a22d58bbd6ab0dca15af38f9c078f66b9207f9d7
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-03 08:39:53 +0300

    added pg

--- src/Contactpg/Contact.js
+++ src/Contactpg/Contact.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Contact.css'
+import { Typography } from '@mui/material'
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contactpage'>
+      <div>
+        <Typography variant='h5' color='darkblue' fontWeight={300}>
+          Contact Me
+        </Typography>
+      </div>
+    </div>
   )
 }
 
